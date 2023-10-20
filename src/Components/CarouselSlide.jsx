aa8bb40c130dd4a99a22edb59554711f583ceca6
saddamseikh <saddamseikh2229@gmail.com>
2023-10-20 21:35:39 +0530

    Carousel previous slide modify

--- src/Components/CarouselSlide.jsx
+++ src/Components/CarouselSlide.jsx
@@ -10,7 +10,7 @@ function CarouselSlide({image, title, description, slideNumber, totalSlides}){
                             </p>
                             <h3 className="text-2xl semibolad">{title}</h3>
                             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                <a href={`#slide${(slideNumber == 1 ? totalSlides : (totalSlides))}`} className="btn btn-circle">❮</a>
+                                <a href={`#slide${(slideNumber == 1 ? totalSlides : (slideNumber -1 ))}`} className="btn btn-circle">❮</a>
                                 <a href={`#slide${(slideNumber) % totalSlides + 1}`} className="btn btn-circle">❯</a>
                             </div>
                         </div>
